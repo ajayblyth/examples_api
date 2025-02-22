02d8dd6d6807fbf0e5524b0aa8e1adb054dd29ea
ajayblyth <ajayblyth@gmail.com>
2025-02-22 18:58:52 +0530

    Validation annotations

--- src/main/java/com/apiexamples/examples/payload/RegistrationDto.java
+++ src/main/java/com/apiexamples/examples/payload/RegistrationDto.java
@@ -2,6 +2,8 @@ package com.apiexamples.examples.payload;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -9,8 +11,11 @@ import lombok.Data;
 
 public class RegistrationDto {
 
+//    Note: @Size(min =1, max = 2) will work for string only
+//          for Integer use  @Min(value = ) and @Max(value =)
+
     private Long id;
-@Size(min = 2, max = 30, message = "name should be between 3-3- characters")
+    @Size(min = 2, max = 30, message = "name should be between 3-3- characters")
     private String name;
 
     private String email;
