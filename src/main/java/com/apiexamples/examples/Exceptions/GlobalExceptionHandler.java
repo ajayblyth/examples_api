6710526186954788fb2086444877bcbb89113059
ajayblyth <ajayblyth@gmail.com>
2025-02-23 13:15:51 +0530

    Relationship Mapping in JPA/Hibernate

--- src/main/java/com/apiexamples/examples/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/apiexamples/examples/Exceptions/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Date;
-
+s
 @ControllerAdvice //This annotation is applied to a class that will handle global exceptions
 public class GlobalExceptionHandler {
 
